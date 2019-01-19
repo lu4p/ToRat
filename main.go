61301e057143a281afcb5a5cf29ae6027b493148
lu4p <lu4p@protonmail.com>
2019-01-19 12:56:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package ToRat
 import(
-  github.com/lu4p/ToRat_client
-  github.com/lu4p/ToRat_server
-  github.com/lu4p/genCert
+  "github.com/lu4p/ToRat_client"
+  "github.com/lu4p/ToRat_server"
+  "github.com/lu4p/genCert"
 )
