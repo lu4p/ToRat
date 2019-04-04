168e29a1ade388d23dd573165cc78c7b73938b01
lu4p <lu4p@protonmail.com>
2019-04-04 23:16:26 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import(
   "github.com/lu4p/genCert"
 )
 
-// This file is for making the go get process easier this servers no other purpose
+// This file is soley for making the go get process easier this serves no other purpose
 
 func main(){
 }
